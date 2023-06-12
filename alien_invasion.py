5d68d3d4c81e9d267a3a86b837254d2c9e4925fd
sokolclaw <kristoferiork@yandex.ru>
2023-06-12 16:36:35 +0500

    Add bullets for ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,45 +4,84 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
 
 class AlienInvasion:
 
     def __init__(self):
+        '''Инициализирует игру и создает игровые ресурсы'''
         pygame.init()
-
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        self.screen = pygame.display.set_mode(
+                        (self.settings.screen_width, self.settings.screen_height)
+                    )
         pygame.display.set_caption('Alien Invasion')
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         while True:
             self._check_events()
             self.ship.moving()
+            self._update_bullets()
             self._update_screen()
 
+            
     def _check_events(self):
         '''Обрабатывает нажатия клавиш и события мыши'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        '''Реагирует на нажатие клавиш'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        if event.key == pygame.K_q:
+            sys.exit()
+        if event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        
+
+    def _check_keyup_events(self, event):
+        '''Реагирует на отпускание клавиш'''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        '''Создание нового снаряда и включение его в группу bullets'''
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+
 
     def _update_screen(self):
         '''Обновляет изображения на экране и отображает новый экран'''
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
-
+        
         pygame.display.flip()
 
 
