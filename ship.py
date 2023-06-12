c77b18bc9528438db8c74bb04900e3cd6f35e1e2
sokolclaw <kristoferiork@yandex.ru>
2023-06-12 14:40:09 +0500

    Create window with a ship

---
+++ ship.py
@@ -0,0 +1,18 @@
+import pygame 
+from pygame import Surface
+
+class Ship():
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        print(type(self.screen))
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+
+
+    
